76a392ee4ea60513be227b5f94ee1fa375a40091
jiageeth <jiageeth@gmail.com>
2026-04-10 16:26:38 +0530

    first

---
+++ cd-1_1/script.js
@@ -0,0 +1,11 @@
+const element=(<div className="superleague-container">
+    <h1 className="head">Hi everyone</h1>
+    <div className="first-cont">
+        <img src="1.jpg"/>
+    </div>
+    <div className="first-cont">
+        <img src="2.jpg"/>
+    </div>
+</div>);
+
+ReactDOM.render(element,document.getElementById("root"));
